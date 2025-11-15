960920492a59727a001c9b29cecf73e90f7f2e4e
Marcelo Lima <91571229+marceloliima@users.noreply.github.com>
2025-11-14 21:51:22 -0300

    Update batalhaNaval.c
    nivel aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,37 +5,58 @@ int main() {
     // Criando o tabuleiro 10x10
     int tabuleiro[10][10];
 
-    // Preenchendo o tabuleiro com 0 (água)
+    // Preenchendo o tabuleiro com 0
     for(int i = 0; i < 10; i++) {
         for(int j = 0; j < 10; j++) {
             tabuleiro[i][j] = 0;
         }
     }
 
-    // Navios com tamanho 3
-    int navioHorizontal[3] = {3, 3, 3};
-    int navioVertical[3] = {3, 3, 3};
+    // Navios tamanho 3
+    int navio1[3] = {3, 3, 3}; // horizontal
+    int navio2[3] = {3, 3, 3}; // vertical
+    int navio3[3] = {3, 3, 3}; // diagonal direita
+    int navio4[3] = {3, 3, 3}; // diagonal esquerda
 
-    // Coordenadas iniciais dos navios
-    // coloquei valores simples
-    int linhaH = 2;
-    int colunaH = 1;
+    // Coordenadas escolhidas
+    // Horizontal
+    int hL = 1;
+    int hC = 2;
 
-    int linhaV = 5;
-    int colunaV = 7;
+    // Vertical
+    int vL = 4;
+    int vC = 7;
 
-    // Posicionando navio horizontal (vai para o lado)
-    tabuleiro[linhaH][colunaH] = 3;
-    tabuleiro[linhaH][colunaH + 1] = 3;
-    tabuleiro[linhaH][colunaH + 2] = 3;
+    // Diagonal direita
+    int d1L = 6;
+    int d1C = 1;
 
-    // Posicionando navio vertical (vai para baixo)
-    tabuleiro[linhaV][colunaV] = 3;
-    tabuleiro[linhaV + 1][colunaV] = 3;
-    tabuleiro[linhaV + 2][colunaV] = 3;
+    // Diagonal esquerda
+    int d2L = 2;
+    int d2C = 8;
 
-    // Exibindo o tabuleiro
-    printf("TABULEIRO NIVEL NOVATO:\n\n");
+    // Posicionando navio horizontal
+    tabuleiro[hL][hC]     = 3;
+    tabuleiro[hL][hC + 1] = 3;
+    tabuleiro[hL][hC + 2] = 3;
+
+    // Posicionando navio vertical
+    tabuleiro[vL][vC]     = 3;
+    tabuleiro[vL + 1][vC] = 3;
+    tabuleiro[vL + 2][vC] = 3;
+
+    // Posicionando navio diagonal descendo para direita
+    tabuleiro[d1L][d1C] = 3;
+    tabuleiro[d1L + 1][d1C + 1] = 3;
+    tabuleiro[d1L + 2][d1C + 2] = 3;
+
+    // Posicionando navio diagonal descendo para esquerda
+    tabuleiro[d2L][d2C] = 3;
+    tabuleiro[d2L + 1][d2C - 1] = 3;
+    tabuleiro[d2L + 2][d2C - 2] = 3;
+
+    // Exibindo o tabuleiro completo
+    printf("TABULEIRO NIVEL AVENTUREIRO:\n\n");
 
     for(int i = 0; i < 10; i++) {
         for(int j = 0; j < 10; j++) {
